4ab9b8cb68eb8b1062305ae7ec3d41e8c455b5f3
DaniloNovaes <107276937+DaniloNovaes@users.noreply.github.com>
2022-07-27 16:46:18 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const btnMobile = document.getElementById('btn-mobile');
 
 
+
 function toggleMenu(event){
 if (event.type === 'touchstart') event.preventDefault();
 const containerNav = document.getElementById('container-nav');
